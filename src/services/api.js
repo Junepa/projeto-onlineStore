40d4958b7cb0f8944a991f7ecf4fe1d746f63ece
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-03-17 16:22:02 -0300

    Requisito 1.2

--- src/services/api.js
+++ src/services/api.js
@@ -15,6 +15,9 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
 }
 
 export async function getProductById() {
-  // Esta implementação específica não é avaliada, mas pode ajudar você
-  // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
+  const API_URL = `https://api.mercadolibre.com/sites/MLB/search?category=${CATEGORY_ID}`;
+
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  return data;
 }
